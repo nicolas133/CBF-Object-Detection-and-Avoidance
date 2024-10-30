fc74690dccd4c318cfc74feb5bd82188dcfbb800
nicolas133 <37260285+nicolas133@users.noreply.github.com>
2024-10-30 14:13:59 -0500

    clustering.py now publishes to cbf_filter

--- Clustering/clustering.py
+++ Clustering/clustering.py
@@ -11,273 +11,243 @@ import matplotlib.cm as cm
 from scipy.spatial import ConvexHull
 from sensor_msgs.msg import PointField
 import matplotlib.patches as patches
-import rospy
+#clusters = []
+
+
 
+def filtering(cloud):
+
+  # Downsample the cloud to lower resolution to make algo run faster 
+  downsampled_cloud = do_voxel_grid_filter(point_cloud = cloud, LEAF_SIZE = 0.01)
+  print("After voxel grid filter:", downsampled_cloud.size)
+
+
+  #Spatial RANGE FILTER-implement  
+  filtered_cloud = do_passthrough_filter(point_cloud = downsampled_cloud, 
+                                         name_axis = 'x', min_axis = -1, max_axis = 1.0)
+
+  filtered_cloud = do_passthrough_filter(point_cloud = downsampled_cloud, 
+                                         name_axis = 'y', min_axis = -1, max_axis = 1)
+  #print("After passthrough filter:", filtered_cloud.size)
+
+ 
+   
+  print("After Bounding Box",filtered_cloud)
+
+  objects_cloud = filtered_cloud
+ 
+# cloud is an array with each cell having three numbers corresponding to x, y, z position
+# Returns list of [x, y, z, color]
+
+  return objects_cloud 
+
+
+
+def db_scan(cloud, tolerance, min_size, max_size,debug=False):
+
+  # 'clusters' is a list of lists, with each incidiy representing the cluster it belongs to 
+
+  tree = cloud.make_kdtree()
+  extraction_object = cloud.make_EuclideanClusterExtraction()
+
+  extraction_object.set_ClusterTolerance(tolerance)
+  extraction_object.set_MinClusterSize(min_size)
+  extraction_object.set_MaxClusterSize(max_size)
+  extraction_object.set_SearchMethod(tree)
+  cluster_indices = extraction_object.Extract()
+  
+  # Get the X and Y data points for the cluster
+  clusters = []
+  points = cloud.to_array()
+  
+  # Run through the indices and get the X, Y coordinates of the points
+  for indices in cluster_indices:
+      cluster_points = points[indices][:, :2]
+      clusters.append(cluster_points)
+
+  # print only when debug 
+  if debug == True:
+      print("There are a total number of:", {len(clusters)}," Clusters")
+  
+     # print(clusters)
+  
+  # Create the graph for the three clusters and save the figure
+  colors = cm.viridis(np.linspace(0, 1, len(clusters)))
+  for i, cluster in enumerate(clusters):
+      plt.scatter(cluster[:,0], cluster[:,1], color = colors[i], alpha=0.6)
+  
+  plt.savefig('dbscan_clusters.png')
+  
+  return cluster_indices, clusters
 
-# clusters = []
+
+def visualize_convex_hull(cloud, clusters):
+	
+	for i in clusters:
+		cluster_points = []
+		cluster_points_x = []
+		cluster_points_y  = []  
+		for j in i:
+			cluster_points.append(j)
+			cluster_points_x.append(j[0])
+			cluster_points_y.append(j[1])
 
-class Clustering_Shaping:
+		print(cluster_points)
+		plt.scatter(cluster_points_x, cluster_points_y, label=f'Cluster {i}', s = 10)
 
-    def __init__(self):
 
-        print("enterd init of clustering")
-        # ROS node initialization
-        rospy.init_node('clustering', anonymous=True)
+		cluster_points_x = np.array(cluster_points_x)
+		cluster_points_y = np.array(cluster_points_y)
 
-        rospy.Subscriber("/point_cloud", PointCloud, pcl_callback, queue_size=1)
+		min_x = cluster_points_x.min()
+		min_y = cluster_points_y.min()
+		max_x = cluster_points_x.max()
+		max_y = cluster_points_y.max()
 
-        # Create Publishers
-        self.clusters_publisher = rospy.Publisher("/pcl_cluster", PointCloud2, queue_size=1)
+		width = max_x + min_x
+		length = max_y + min_y
 
-        self.wrapped_obj_pub = rospy.Publisher("WrappedObjs",Twist,queue_size=1)
+		centroid_x = width/2
+		centroid_y = length/2
+		centroid_pair = (centroid_x, centroid_y)
+		
+		#width_length_pair = (abs(width), abs(length))
+		#radius = max(width_length_pair)/2
 
-
-
-        # Initialize color_list
-        get_color_list.color_list = []
-
-
-    def filtering(cloud):
-        # Downsample the cloud to lower resolution to make algo run faster
-        downsampled_cloud = do_voxel_grid_filter(point_cloud=cloud, LEAF_SIZE=0.01)
-        print("After voxel grid filter:", downsampled_cloud.size)
-
-        # Spatial RANGE FILTER-implement
-        filtered_cloud = do_passthrough_filter(point_cloud=downsampled_cloud,
-                                               name_axis='x', min_axis=-1, max_axis=1.0)
-
-        filtered_cloud = do_passthrough_filter(point_cloud=downsampled_cloud,
-                                               name_axis='y', min_axis=-1, max_axis=1)
-        # print("After passthrough filter:", filtered_cloud.size)
-
-        print("After Bounding Box", filtered_cloud)
-
-        objects_cloud = filtered_cloud
-
-        # cloud is an array with each cell having three numbers corresponding to x, y, z position
-        # Returns list of [x, y, z, color]
-
-        return objects_cloud
-
-
-    def db_scan(cloud, tolerance, min_size, max_size, debug=False):
-        # 'clusters' is a list of lists, with each incidiy representing the cluster it belongs to
-
-        tree = cloud.make_kdtree()
-        extraction_object = cloud.make_EuclideanClusterExtraction()
-
-        extraction_object.set_ClusterTolerance(tolerance)
-        extraction_object.set_MinClusterSize(min_size)
-        extraction_object.set_MaxClusterSize(max_size)
-        extraction_object.set_SearchMethod(tree)
-        cluster_indices = extraction_object.Extract()
-
-        #cluster inidices cluster_indices[0] = [0, 1, 2] means points at indices 0, 1, and 2 in cloud belong to cluster 0.
-
-
-        # Get the X and Y data points for the cluster
-        clusters = []
-
-        points = cloud.to_array() #convert to numpy array but shape is stll nx4
-
-        # Run through the indices and get the X, Y coordinates of the points
-        for indices in cluster_indices:
-            # for cluster 0 go to 1,2,3 the rows which have points that belong to cluster 0
-            cluster_points = points[indices][:, 0:2] #grab all rows but only grab columns 0 and 1 up to but not including 2
-
-            clusters.append(cluster_points) # now we have xy array
-
-
-        # print only when debug
-        if debug == True:
-            print("There are a total number of:", {len(clusters)}, " Clusters")
-
-        # print(clusters)
-
-        # Create the graph for the three clusters and save the figure
-        colors = cm.viridis(np.linspace(0, 1, len(clusters)))
-
-        fig1, ax =plt.subplots()
-
-        for i, cluster in enumerate(clusters):
-            ax.scatter(cluster[:, 0], cluster[:, 1], color=colors[i], alpha=0.6)
-
-        plt.savefig('dbscan_clusters.png')
-
-        return cluster_indices, clusters
-
-
-    def bounding_box(cloud, clusters):
-        for i in clusters:
-            cluster_points = []
-            cluster_points_x = []
-            cluster_points_y = []
-            for j in i:
-                cluster_points.append(j)
-                cluster_points_x.append(j[0])
-                cluster_points_y.append(j[1])
-
-            print(cluster_points)
-
-            fig, ax2 = plt.subplots()
-            ax2.scatter(cluster_points_x, cluster_points_y, label=f'Cluster {i}', s=10)
-
-            cluster_points_x = np.array(cluster_points_x)
-            cluster_points_y = np.array(cluster_points_y)
-
-            min_x = cluster_points_x.min()
-            min_y = cluster_points_y.min()
-            max_x = cluster_points_x.max()
-            max_y = cluster_points_y.max()
-
-            width = max_x - min_x
-            length = max_y - min_y
-
-            half_width = width/2
-            half_length = length/2
-
-            centroid_x = (max_x + min_x) / 2
-            centroid_y = (max_y + min_y) / 2
-            centroid_pair = (centroid_x, centroid_y)
-
-            # width_length_pair = (abs(width), abs(length))
-            # radius = max(width_length_pair)/2
-
-            #np_max_pair = ([half_width,half_length])
-            # if use this then (0,0) is second point in Euclidian distance calucation
-            # as this is already distance refrenced from centroid
-
-            np_max_pair = np.array(max_x, max_y)
-
-            np_centroid_pair = np.array(centroid_pair)
-
-            radius = np.linalg.norm(np_centroid_pair - np_max_pair)
-
-            print("Centroid x: ", centroid_x)
-            print("Centroid y: ", centroid_y)
-            print("Centroid pair: ", centroid_pair)
-            print("radius: ", radius)
-
-            print("min and min" + str(min_x) + "," + str(min_y))
-            print("max and max" + str(max_x) + "," + str(max_y))
-
-           # rectangle = np.array([[min_x, min_y], [max_x, min_y], [max_x, max_y], [min_x, max_y]])##npt used to print anymore
-
-            #fig, ax = plt.subplots()
-            circle = patches.Circle(centroid_pair, radius, fill=False, edgecolor="blue")
-            rectangle= patches.Rectangle((min_x,min_y),width,length,linewidth=1, edgecolor='r', facecolor='none',fill=False)
-
-
-            ax2.add_patch(rectangle)
-            ax2.add_patch(circle)
-
-            wrapped_objs = Twist()
-            wrapped_objs.linear.x = centroid_x
-            wrapped_objs.linear.y = centroid_y
-            wrapped_objs.linear.z = radius
-            self.wrapped_obj_pub(wrapped_objs)
-
-
-            #plt.fill(rectangle[:, 0], rectangle[:, 1], linewidth=2, alpha=0.5, label=f'Cluster {i}')
-
-
-
-        # save final plot
-        plt.savefig('clusters_with_bounding_box')
-
-
-
-
-        return
-
-
-    # clusters is a list of lists each list containing indices of the cloud
-
-    def get_colored_clusters(clusters, cloud):
-        # Get a random unique colors for each object
-        number_of_clusters = len(clusters)
-        colors = get_color_list(number_of_clusters)
-
-        colored_points = []
-
-        # Assign a color for each point
-        # Points with the same color belong to the same cluster
-        for cluster_id, cluster in enumerate(clusters):
-            for c, i in enumerate(cluster):
-                x, y, z = cloud[i][0], cloud[i][1], cloud[i][2]
-                color = rgb_to_float(colors[cluster_id])
-                colored_points.append([x, y, z, color])
-
-        return colored_points
-
-
-    # Callback function for your Point Cloud Subscriber
-    def pcl_callback(self,pcl_msg):
-        print("entered pcl_callback")
-
-        pcl2_msg = pointcloud_to_pointcloud2(pcl_msg)  # convert to pcl2
-
-        # Convert ROS msg to PCL data
-        cloud = ros_to_pcl(pcl2_msg)
-
-        frame_id = pcl2_msg.header.frame_id
-
-        print("Initial__cloud size:", cloud.size)
-
-        # IMPLEMENT BOUNDING BOX AND LOWER RESOLUTION
-        objects_cloud = filtering(cloud)
-
-        # Get a point cloud of only the position information without color information
-        colorless_cloud = XYZRGB_to_XYZ(objects_cloud)
-
-        # This is effectively a list of lists, with each list containing indices of the cloud
-        clusters_indicies, clusters = db_scan(colorless_cloud, tolerance=0.05, min_size=20, max_size=1500, debug=True)
-
-        bounding_box(cloud, clusters)
-
-        # Assign a unique color float for each point (x, y, z)
-        # Points with the same color belong to the same cluster
-        colored_points = get_colored_clusters(clusters, colorless_cloud)
-
-        # Create a cloud with each cluster of points having the same color
-        clusters_cloud = pcl.PointCloud_PointXYZRGB()
-        clusters_cloud.from_list(colored_points)
-
-        # Convert pcl data to ros messages
-
-        clusters_msg = pcl_to_ros(clusters_cloud, frame_id)
-
-        # Publish ROS messages
-        self.clusters_publisher.publish(clusters_msg)
-
-
-    def pointcloud_to_pointcloud2(pointcloud_msg):
-        header = pointcloud_msg.header
-
-        # Define the fields for PointCloud2, including 'rgb'
-        fields = [
-            PointField('x', 0, PointField.FLOAT32, 1),
-            PointField('y', 4, PointField.FLOAT32, 1),
-            PointField('z', 8, PointField.FLOAT32, 1),
-            PointField('rgb', 12, PointField.FLOAT32, 1),
-        ]
-
-        # Create a list of points with default RGB values
-        points = []
-        for point in pointcloud_msg.points:
-            x, y, z = point.x, point.y, point.z
-            # Set default color to white (255, 255, 255)
-            r, g, b = 255, 255, 255
-            rgb = (int(r) << 16) | (int(g) << 8) | int(b)
-            rgb_float = struct.unpack('f', struct.pack('I', rgb))[0]
-            points.append([x, y, z, rgb_float])
-
-        # Create PointCloud2 message
-        pcl2_msg = point_cloud2.create_cloud(header, fields, points)
-        return pcl2_msg
-
+		if (width > length) :
+			np_max_pair = np.array([max_x,0])
+		else:
+			np_max_pair = np.array([0,max_y])
+		np_max_pair = np.array(max_x, max_y)
+		np_centroid_pair = np.array(centroid_pair)
+
+		radius = np.linalg.norm(np_centroid_pair - np_max_pair)
+
+		print("Centroid x: " , centroid_x)
+		print("Centroid y: " , centroid_y)
+		print("Centroid pair: " , centroid_pair)
+		print("radius: " , radius)
+
+		print("min and min" + str(min_x) + "," + str(min_y))
+		print("max and max"+ str(max_x)  + "," + str(max_y))
+             
+                	
+
+		rectangle = np.array([[min_x, min_y], [max_x, min_y], [max_x, max_y], [min_x, max_y]])
+		fig,ax =plt.subplots()
+		circle=patches.Circle(centroid_pair,radius,fill=False,edgecolor="blue")	
+		ax.add_patch(circle)
+		plt.savefig("circle_image")
+		
+
+		plt.fill(rectangle[:,0], rectangle[:,1], linewidth=2,alpha=0.5, label=f'Cluster {i}')
+	#plt.show()
+	plt.savefig('Shape_Wrapping.png')
+	return
+  
+
+# clusters is a list of lists each list containing indices of the cloud
+
+def get_colored_clusters(clusters, cloud):
+  
+  # Get a random unique colors for each object
+  number_of_clusters = len(clusters)
+  colors = get_color_list(number_of_clusters)
+
+  colored_points = []
+
+  # Assign a color for each point
+  # Points with the same color belong to the same cluster
+  for cluster_id, cluster in enumerate(clusters):
+    for c, i in enumerate(cluster):
+      x, y, z = cloud[i][0], cloud[i][1], cloud[i][2]
+      color = rgb_to_float(colors[cluster_id])
+      colored_points.append([x, y, z, color])
+  
+  return colored_points
+
+
+# Callback function for your Point Cloud Subscriber
+def pcl_callback(pcl_msg):
+
+  print("entered pcl_callback")
+
+  pcl2_msg = pointcloud_to_pointcloud2(pcl_msg) #convert to pcl2
+
+  # Convert ROS msg to PCL data
+  cloud = ros_to_pcl(pcl2_msg) 
+
+  frame_id = pcl2_msg.header.frame_id
+
+  print("Initial__cloud size:", cloud.size)
+
+
+  # IMPLEMENT BOUNDING BOX AND LOWER RESOLUTION 
+  objects_cloud = filtering(cloud) 
+
+  # Get a point cloud of only the position information without color information
+  colorless_cloud = XYZRGB_to_XYZ(objects_cloud)
+  
+
+  # This is effectively a list of lists, with each list containing indices of the cloud
+  clusters_indicies, clusters = db_scan(colorless_cloud, tolerance = 0.05, min_size = 20, max_size = 1500,debug=True)
+  
+  visualize_convex_hull(cloud, clusters)
+
+  # Assign a unique color float for each point (x, y, z)
+  # Points with the same color belong to the same cluster
+  colored_points = get_colored_clusters(clusters, colorless_cloud)
+
+  # Create a cloud with each cluster of points having the same color
+  clusters_cloud = pcl.PointCloud_PointXYZRGB()
+  clusters_cloud.from_list(colored_points)
+
+  # Convert pcl data to ros messages
+ 
+  clusters_msg = pcl_to_ros(clusters_cloud, frame_id)
+
+  # Publish ROS messages
+  clusters_publisher.publish(clusters_msg)
+ 
+def pointcloud_to_pointcloud2(pointcloud_msg):
+    header = pointcloud_msg.header
+
+    # Define the fields for PointCloud2, including 'rgb'
+    fields = [
+        PointField('x', 0, PointField.FLOAT32, 1),
+        PointField('y', 4, PointField.FLOAT32, 1),
+        PointField('z', 8, PointField.FLOAT32, 1),
+        PointField('rgb', 12, PointField.FLOAT32, 1),
+    ]
+
+    # Create a list of points with default RGB values
+    points = []
+    for point in pointcloud_msg.points:
+        x, y, z = point.x, point.y, point.z
+        # Set default color to white (255, 255, 255)
+        r, g, b = 255, 255, 255
+        rgb = (int(r) << 16) | (int(g) << 8) | int(b)
+        rgb_float = struct.unpack('f', struct.pack('I', rgb))[0]
+        points.append([x, y, z, rgb_float])
+
+    # Create PointCloud2 message
+    pcl2_msg = point_cloud2.create_cloud(header, fields, points)
+    return pcl2_msg
 
 if __name__ == '__main__':
-    print("new callback run--------------------------------------------------------------")
-    Detected_Objects = Clustering_Shaping()
+  print("enterd main of clustering")
+  # ROS node initialization
+  rospy.init_node('clustering', anonymous = True)
+
+    
+  subscriber = rospy.Subscriber("/point_cloud", PointCloud, pcl_callback, queue_size = 1)
+
+  # Create Publishers
+  clusters_publisher = rospy.Publisher("/pcl_cluster", PointCloud2, queue_size = 1)
+  
+  # Initialize color_list
+  get_color_list.color_list = []
+
+  # Spin while node is not shutdown
+  while not rospy.is_shutdown():
     rospy.spin()
